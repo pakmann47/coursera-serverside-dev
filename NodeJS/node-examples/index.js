ad4f02d83fd5fc5b35db07c9466e4c3d57eb74d2
David <dreyes.its-b2013@fh-salzburg.ac.at>
2018-10-30 23:09:56 +0100

    first node module - rect

---
+++ NodeJS/node-examples/index.js
@@ -0,0 +1,22 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect (l, b) {
+    console.log("solving rect with l = " + l + " and b = " + b);
+
+    if (l <= 0 || b <= 0) {
+        console.log("<= 0? seriously?");
+    } else {
+        console.log("Perimerter: " + rect.perimeter(l, b));
+        console.log("Area: " + rect.area(l, b));
+    }
+    
+    console.log();
+}
+
+solveRect(0, 1);
+solveRect(1, -1);
+solveRect(2, 4);
+solveRect(10, 9);
